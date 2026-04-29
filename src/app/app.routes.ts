59afebdb04d3c01da749bface204beca33c917da
Agustin Soria <soria.agustin@hotmail.com>
2026-04-28 22:16:51 -0300

    Initial Angular Project Setup fopr Cordoba Se Plkanta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './sections/inicio/inicio';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+
+  {
+    path: 'historia',
+    loadComponent: () =>
+      import('./sections/historia/historia').then(m => m.Historia),
+  },
+
+  { path: '**', redirectTo: '' },
+];
